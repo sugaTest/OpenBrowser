396d69921cdb79a445c04e14d93b8777d74249bd
Suga Test <115866585+sugaTest@users.noreply.github.com>
2024-02-10 14:59:28 +0530

    Add files via upload

---
+++ src/test/java/org/example/openBrowser.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class openBrowser {
+
+    @Test
+    public void openBrowser() {
+        System.out.println("Hello and welcome!");
+    }
+}
